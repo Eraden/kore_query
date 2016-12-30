d49bede98c9b938c391ddf0fce2a8385b8096344
Eraden <adrian.wozniak.1986@gmail.com>
2016-12-30 11:10:12 +0100

    Fix cmake on circle ci

--- .circle/install.sh
+++ .circle/install.sh
@@ -3,13 +3,15 @@
 echo "$(lsb_release -a)"
 
 sudo apt-get --yes update &> /dev/null
+sudo apt-get --yes install language-pack-pl
 sudo apt-get --yes install check
 sudo apt-get --yes remove clang
+sudo apt-get --yes remove cmake
 sudo apt-get --yes install clang-3.8
 sudo apt-get --yes install libyajl-dev
 sudo apt-get --yes install libyajl2
 sudo apt-get --yes install libpq-dev
-sudo apt-get --yes install postgresql-server-dev-9.6
+sudo apt-get --yes install libpq-dev postgresql-server-dev-all
 sudo apt-get --yes install kore
 sudo apt-get --yes install lcov
 
@@ -34,39 +36,39 @@ then
     mkdir -p ${root}/tmp
 fi
 
-#if [[ "$(cmake --version | grep -E '[0-9]+.[0-9]+.[0-9]+' | sed 's/[a-z ]//gi')" == "3.6.1" ]]
-#then
-#  echo "Valid cmake"
-#else
-#  if [[ ! -f ${root}/vendor/cmake-3.6.1 ]]
-#  then
-#    cd vendor
-#    wget https://cmake.org/files/v3.6/cmake-3.6.1.tar.gz
-#    tar -xvf cmake-3.6.1.tar.gz &> /dev/null
-#    cd ${root}/vendor/cmake-3.6.1
-#    echo "Configure cmake..."
-#    ./configure &> /dev/null
-#    echo "Building cmake..."
-#    make -j 4 &> /dev/null
-#    cd ${root}
-#  fi
-#  cd ${root}/vendor/cmake-3.6.1
-#  echo "Installing cmake..."
-#  sudo make install &> /dev/null
-#  echo "  done"
-#  cd ${root}
-#fi
-#
-#export PATH="/usr/local/bin:/usr/bin:$PATH"
-#export PATH="/usr/lib/llvm-3.8/bin:$PATH"
-#
-#version="$(cmake --version | grep -E '[0-9]+.[0-9]+.[0-9]+' | sed 's/[a-z ]//gi')"
+if [[ "$(cmake --version | grep -E '[0-9]+.[0-9]+.[0-9]+' | sed 's/[a-z ]//gi')" == "3.6.1" ]]
+then
+  echo "Valid cmake"
+else
+  if [[ ! -d ${root}/vendor/cmake-3.6.1 ]]
+  then
+    cd vendor
+    wget https://cmake.org/files/v3.6/cmake-3.6.1.tar.gz
+    tar -xvf cmake-3.6.1.tar.gz &> /dev/null
+    cd ${root}/vendor/cmake-3.6.1
+    echo "Configure cmake..."
+    ./configure &> /dev/null
+    echo "Building cmake..."
+    make -j 4 &> /dev/null
+    cd ${root}
+  fi
+  cd ${root}/vendor/cmake-3.6.1
+  echo "Installing cmake..."
+  sudo make install &> /dev/null
+  echo "  done"
+  cd ${root}
+fi
+
+export PATH="/usr/local/bin:/usr/bin:$PATH"
+export PATH="/usr/lib/llvm-3.8/bin:$PATH"
+
+version="$(cmake --version | grep -E '[0-9]+.[0-9]+.[0-9]+' | sed 's/[a-z ]//gi')"
 echo "cmake version: '$version'"
 
-#if [[ "$version" == "3.6.1" ]]
-#then
-#  echo "CMake is valid"
-#else
-#  echo "Failed to install cmake"
-#  exit 1
-#fi
+if [[ "$version" == "3.6.1" ]]
+then
+  echo "CMake is valid"
+else
+  echo "Failed to install cmake"
+  exit 1
+fi
--- .circle/memory_check.sh
+++ .circle/memory_check.sh
@@ -3,10 +3,8 @@
 export PATH="/usr/local/bin:$PATH"
 export PATH="/usr/lib/llvm-3.8/bin:$PATH"
 
-export CMAKE_C_COMPILER=$(which clang)
-export CMAKE_CXX_COMPILER=$(which clang++)
-export CPP=$(which clang++)
-export CC=$(which clang)
+export CMAKE_C_COMPILER=$(which clang-3.8)
+export CC=$(which clang-3.8)
 
 echo "clang path: $(which clang)"
 
@@ -15,6 +13,6 @@ git submodule update --init --recursive
 echo "  clone submodules done"
 
 cd build
-cmake -DCMAKE_C_COMPILER=$(which clang) -DCMAKE_CXX_COMPILER=$(which clang++) -DVALIDATE_MEMORY=FALSE -DVALIDATE_ADDRESS=FALSE -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCMAKE_C_COMPILER=$(which clang-3.8) -DCMAKE_CXX_COMPILER=$(which clang++-3.8) -DCMAKE_BUILD_TYPE=Debug ..
 make -j 4
 ./test_kore_query
