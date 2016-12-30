0bd61b80e64da642dcfa80a13adf1b70d88f83b2
Eraden <adrian.wozniak@ita-prog.pl>
2016-12-30 22:17:02 +0100

    Remove unused files

--- cmake/GenerateBadges.cmake
+++
@@ -1,9 +0,0 @@
-SET(coverage_info "${CMAKE_BINARY_DIR}/${_outputname}.info")
-SET(coverage_cleaned "${coverage_info}.cleaned")
-SET(coverage_report "${coverage_info}.report")
-SET(coverage_badges "${CMAKE_CURRENT_SOURCE_DIR}/.badges")
-
-ADD_CUSTOM_COMMAND(
-    COMMAND bash ${CMAKE_CURRENT_SOURCE_DIR}/cmake/create_badges.sh ${coverage_report} ${coverage_badges}
-)
-
